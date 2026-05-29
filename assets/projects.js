87dcd201710ca2ffed54f127ba9565e5715dd815
Anhedonie-69 <brunomartignat@gmail.com>
2026-05-29 09:18:54 +0200

    Version_2

--- assets/projects.js
+++ assets/projects.js
@@ -13,22 +13,29 @@ const projects = [
     title: "Drum-Pad",
     description: "Boite à rhytme sommaire",
     tech: ["HTML", "CSS", "JS"],
-    img: "Images/red_case.png",
+    img: "assets/Images/red_case.png",
     link: "https://anhedonie-69.github.io/DrumPad/"
   },
   {
-    title: "Projet 2",
-    description: "Description courte",
-    tech: ["JS", "API"],
-    img: "Images/blue_case.png",
+    title: "Alpine",
+    description: "Configurateur de véhicule automobile",
+    tech: ["HTML", "CSS", "React", "Tailwind"],
+    img: "assets/Images/blue_case.png",
     link: "lien internet"
   },
   {
-    title: "Projet 3",
-    description: "Description courte",
-    tech: ["JS", "API"],
-    img: "Images/green_case.png",
-    link: "lien internet"
+    title: "Cagnotte",
+    description: "Cagnotte en ligne",
+    tech: ["HTML", "CSS", "PHP", "Symfony", "MySQL"],
+    img: "assets/Images/green_case.png",
+    link: "https://bruno-payetonpote.dwwm.garage404.com/"
+  },
+  {
+    title: "Météo",
+    description: "API météo",
+    tech: ["HTML", "CSS", "React"],
+    img: "assets/Images/purple_case.png",
+    link: "..."
   }
 ];
 
@@ -40,7 +47,7 @@ projects.forEach(projet => {
 
     card.innerHTML = `
         <a href="${projet.link}" target="_blank">
-            <h2>${projet.title}</h3>
+            <h2>${projet.title}</h2>
             <p class="para">${projet.description}</p>
             <img src="${projet.img}">
         </a>
